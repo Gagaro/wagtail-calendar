cd4c74b1f797e21389e7e0da5e144ea267129747
Gagaro <gagaro42@gmail.com>
2018-02-03 11:01:02 +0100

    1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='wagtail-calendar',
-    version='1.0',
+    version='1.0.1',
     description='A planning calendar for wagtail',
     long_description=readme(),
     url='https://github.com/Gagaro/wagtail-calendar',
